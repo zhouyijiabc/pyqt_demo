8e6b6af20f074c7087962cad0587f9952dbb0162
xzhouinfo <124373474@qq.com>
2022-01-24 16:45:31 +0800

    new

--- 类和面向对象/class_demo1.py
+++ 类和面向对象/class_demo1.py
@@ -1,15 +1,13 @@
 class PageView:
-    def __init__(self, data_list, page, per_page_num=10):
+    def __init__(self, all_data_list, input_page, view_page_num=10):
         """
+        data_list: 总数据
         page: 当前要查看的页面
         per_page_num: 每页默认显示数据行数
-
-
         """
-
-        self.data_list = data_list
-        self.page = page
-        self.per_page_num = per_page_num
+        self.data_list = all_data_list
+        self.page = input_page
+        self.per_page_num = view_page_num
 
     @property
     def start(self):
