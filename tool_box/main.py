e5fcd643feade4b336e093959cb681b260c9b2e1
python <124373474@qq.com>
2022-02-11 12:49:07 +0800

    增加状态显示

--- tool_box/main.py
+++ tool_box/main.py
@@ -15,11 +15,22 @@ class Run:
         self.set_time = str(self.window.shutdown_timeEdit.time().toPyTime())
         print(self.set_time)
 
-        self.window.shutdown_timeEdit.timeChanged.connect(self.get_shutdown_time)
-        self.window.create_plan_pushButton.clicked.connect(self.create_shutdown)
-        self.window.cancel_pushButton.clicked.connect(self.cancel_shutdown)
+        self.window.shutdown_timeEdit.timeChanged.connect(self.get_shutdown_time) # 获取关机时间
+        self.window.create_plan_pushButton.clicked.connect(self.create_shutdown) # 创建关机计划
+        self.window.cancel_pushButton.clicked.connect(self.cancel_shutdown) # 取消关机计划
 
-        self.window.get_wechat_path_pushButton.clicked.connect(self.get_wechat_path)
+        self.window.get_wechat_path_pushButton.clicked.connect(self.get_wechat_path) # 获取微信路径
+
+        self.window.start_wechat_pushButton.clicked.connect(self.open_wechat)# 开启微信
+
+        self.marge_path = os.getcwd()
+        self.window.marge_path_lineEdit.setText(self.marge_path)
+        self.window.custom_path_radioButton.clicked.connect(self.open_get_marge_btu) # 开启自定义路径按钮
+        self.window.default_path_radioButton.clicked.connect(self.close_get_marge_btu) # 关闭自定义路径按钮
+        self.window.get_marge_path_pushButton.clicked.connect(self.get_marge_path) # 获取合并文件夹路径
+
+
+
 
         self.apply_stylesheet = apply_stylesheet(self.app, theme='dark_teal.xml')
         self.main.show()
@@ -46,6 +57,29 @@ class Run:
         path = QFileDialog.getOpenFileName(None, '请选择微信执行文件')
         print(path[0])
         self.window.wechat_lineEdit.setText(path[0])
+
+    def open_wechat(self):
+        num = self.window.open_num_spinBox.value()
+        wechat_path = self.window.wechat_lineEdit.text()
+        print(num, type(num), wechat_path)
+        command = f'start {wechat_path}'
+        for i in range(num):
+            os.system(command)
+
+    def open_get_marge_btu(self):
+        self.window.get_marge_path_pushButton.setEnabled(True)
+
+    def close_get_marge_btu(self):
+        self.window.get_marge_path_pushButton.setEnabled(False)
+        self.marge_path = os.getcwd()
+        self.window.marge_path_lineEdit.setText(self.marge_path)
+        self.window.statusbar.showMessage(f'当前合并目录为   {self.marge_path}', 5000)
+
+    def get_marge_path(self):
+        self.marge_path = QFileDialog.getExistingDirectory(None, '请选择合并路径')
+        self.window.marge_path_lineEdit.setText(self.marge_path)
+        print(self.marge_path)
+        self.window.statusbar.showMessage(f'设置合并目录为   {self.marge_path}', 5000)
 
 
 if __name__ == '__main__':
